98c99edb001d1e0d78aa6dc1c4450cb13b7ef17e
Lenovo <Lenovo@Lenovo-PC>
2022-03-23 18:02:56 +0530

    first github commit

---
+++ Support/src/test/java/testScripts/CreateClass1.java
@@ -0,0 +1,10 @@
+package testScripts;
+
+public class CreateClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("class is running successfully");
+	}
+
+}
---
+++ Support/src/test/java/testScripts/CreateOrgTest.java
@@ -0,0 +1,9 @@
+package testScripts;
+import org.testng.annotations.Test;
+public class CreateOrgTest {
+	@Test
+public void methodFirst() {
+	System.out.println("create org is running sucessfully");
+	
+}
+}
