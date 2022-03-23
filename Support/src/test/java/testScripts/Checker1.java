d122c65739db023f6d4ea7941c98c3078193ee81
Lenovo <Lenovo@Lenovo-PC>
2022-03-23 18:12:32 +0530

    data check

---
+++ Support/src/test/java/testScripts/Checker1.java
@@ -0,0 +1,10 @@
+package testScripts;
+
+public class Checker1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("firdt git hub data checking");
+	}
+
+}
