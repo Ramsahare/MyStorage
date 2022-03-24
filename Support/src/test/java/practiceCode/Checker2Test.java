778f95879d80e302b2d46e83d391a42704a2496a
Lenovo <Lenovo@Lenovo-PC>
2022-03-24 07:56:41 +0530

    First Trial Pushing code into github MyStorage repository

---
+++ Support/src/test/java/practiceCode/Checker2Test.java
@@ -0,0 +1,10 @@
+package practiceCode;
+
+public class Checker2Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("checker 2 is running sucessfully");
+	}
+
+}
