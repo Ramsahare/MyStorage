d754e1baf25b1b99f3ba065d17dd73c308b1d46d
Lenovo <Lenovo@Lenovo-PC>
2022-03-28 17:27:10 +0530

    UPDATED

--- GITHUBIMPORT/src/test/java/testScipts/Import1.java
+++ GITHUBIMPORT/src/test/java/testScipts/Import1.java
@@ -8,5 +8,6 @@ public class Import1 {
 public void add() {
 	System.out.println("BRANCH CHECKING of githubimport prject  ");
 	System.out.println("pipiline job");
+	System.out.println("DEMO_2_SEDT_31 IS RUNNING SUCCESSFULLY" );
 }
 }
